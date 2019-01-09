a372ff98442670cb8798616cb3fd823ed7355b30
Mike Upton <uptonm@wit.edu>
2019-01-08 20:21:03 -0500

    Work on recipies and recipie list

--- src/app/recipies/recipie-list/recipie-list.component.ts
+++ src/app/recipies/recipie-list/recipie-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipie } from "../recipie.model";
 
 @Component({
-  selector: 'app-recipie-list',
-  templateUrl: './recipie-list.component.html',
-  styleUrls: ['./recipie-list.component.css']
+  selector: "app-recipie-list",
+  templateUrl: "./recipie-list.component.html",
+  styleUrls: ["./recipie-list.component.css"]
 })
 export class RecipieListComponent implements OnInit {
+  recipies: Recipie[] = [
+    new Recipie(
+      "Test Recipie Title",
+      "Test Recipie Description",
+      "https://www.rd.com/wp-content/uploads/2018/04/9-Foods-You-Should-Never-Eat-Before-Bed-760x506.jpg"
+    ),
+    new Recipie(
+      "Test Recipie Title",
+      "Test Recipie Description",
+      "https://www.rd.com/wp-content/uploads/2018/04/9-Foods-You-Should-Never-Eat-Before-Bed-760x506.jpg"
+    )
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
