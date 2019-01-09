0b496d1495f0b75d3be235c66e25af445bb762d3
Mike Upton <uptonm@wit.edu>
2019-01-08 19:37:45 -0500

    Initialized all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { RecipiesComponent } from "./recipies/recipies.component";
+import { RecipieListComponent } from "./recipies/recipie-list/recipie-list.component";
+import { RecipieDetailComponent } from "./recipies/recipie-detail/recipie-detail.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
+import { RecipieItemComponent } from "./recipies/recipe-list/recipie-item/recipie-item.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipieListComponent,
+    RecipieDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipieItemComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
